959250ca2d7b8d0c5ec7d1d0867bdf046cd7a66e
waleed20124 <136029331+waleed20124@users.noreply.github.com>
2025-07-05 23:26:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let add = document.querySelector("#add");
 let add_btn = document.querySelector(".btn-add");
 let ul = document.querySelector("ul");
-let i = 0;
+
 function removeTask(elm) {
   elm.parentNode.removeChild(elm);
-  i-=1;
+  updateNumbers();
+}
+
+function updateNumbers() {
+  let allSpans = ul.querySelectorAll("li span");
+  allSpans.forEach((span, index) => {
+    span.textContent = index + 1;
+  });
 }
 
 function addTask() {
-  
   let inputA = document.createElement("input");
   let btn_remove = document.createElement("button");
   let li = document.createElement("li");
@@ -17,37 +23,28 @@ function addTask() {
 
   inputA.setAttributeNode(atr);
 
-
-  
-
   let text = add.value.trim();
   if (text !== "") {
     inputA.value = text;
     btn_remove.textContent = "Remove";
-    span.textContent = `${++i}`;
     btn_remove.classList.add("btn-remove");
+
     li.appendChild(span);
     li.appendChild(inputA);
     li.appendChild(btn_remove);
     ul.append(li);
 
-    add.value = "";
-
   
+    updateNumbers();
+
+    add.value = "";
   }
 
-  document.querySelector(".btn_remove");
   btn_remove.addEventListener("click", function () {
     removeTask(li);
-     
-        
   });
 }
 
 add_btn.addEventListener("click", function () {
   addTask();
-   
 });
-
-
- 
